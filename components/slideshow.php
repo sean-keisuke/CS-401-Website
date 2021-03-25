2c06d33a80ff021d75e66773aac593a902421c9e
Sean Mullarkey <seanmullarkey@Seans-MBP.hitronhub.home>
2021-03-24 19:49:48 -0600

    js refactor

--- components/slideshow.php
+++ components/slideshow.php
@@ -18,31 +18,6 @@ function getSlideShow($imgArray)
         }
         ?>
     </div>
-
-    <script>
-        $(function () {
-            var slideIndex = 1;
-            showDivs(slideIndex);
-            $(".mySlides").click(function() {
-                showDivs(slideIndex += 1);
-            })
-
-            function showDivs(n) {
-                var i;
-                var picArray = $(".mySlides");
-                if (n > picArray.length) {
-                    slideIndex = 1 // reset back to the first slide
-                }
-                if (n < 1) {
-                    slideIndex = picArray.length
-                }
-                for (i = 0; i < picArray.length; i++) {
-                    $(picArray[i]).css("display", "none")
-                }
-                $(picArray[slideIndex - 1]).css("display", "block")
-            }
-        })
-    </script>
 <?php
 }
 ?>
